6fce075dbf6e5234a6a7bf8fb9347aec0b6e2b84
nietovaca <nietovaca@gmail.com>
2022-01-27 10:38:49 -0700

    day 2 morning

---
+++ VNT.js
@@ -0,0 +1,9 @@
+//used with help from: https://www.w3schools.com
+function openNav() {
+  const nav = document.getElementById("navlinks");
+  if (nav.style.display === "block") {
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "block";
+  }
+}
