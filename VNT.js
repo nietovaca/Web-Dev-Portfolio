a87703772161a0fa2f4dff943b63cbcd1c71f0bc
nietovaca <nietovaca@gmail.com>
2022-01-29 15:51:33 -0700

    resume pg complete

--- VNT.js
+++ VNT.js
@@ -16,69 +16,43 @@
 
 $(()=>{
 
+//with help from SkillForge YouTube tutorial: https://www.youtube.com/watch?v=1cAgfPb6a_8
 $('.accordionItem').accordion({
   "animate":true,
-  "collapsible":true
+  "collapsible":true,
+  active: 1,
+  "heightStyle": "content",
+  "event": "mouseover"
+
 });
 
-//top nav function
+//Nav with help from: https://www.w3schools.com
 $('.topnav').click(function() {
    $('.topnav').not(this).find('#navlinks').hide();
    $(this).find('#navlinks').toggle();
 });
 
-// $('.ghost-button').accordion({
-//   "animate": true,
-//   "collapsible": true
-// })
+//see about adding a timer for hiding
 $('.ghost-button').on('click', () => {
   $('#contactContent').show()
 });
 
 });
 
-//
-// const $accordionTitles = $('.accordionTitle')
-// $accordionTitles.each((accordionTitle) => {
-//   accordionTitle.on('click', () => {
-//     accordionTitle.toggleClass('open')
-//   })
-// })
-
-// const $nav = $('#navlinks');
-// $nav.on('click', () => {
-//   if ($nav.css('display', 'block')) {
-//       $nav.css('display', 'none')
-//   } else {
-//     $nav.css('display', 'block')
-//   }
-// } );
-
-//Nav with help from: https://www.w3schools.com
-            // function openNav(){
-            //   const nav = $('#navlinks');
-            //   if (nav.style.display === "block") {
-            //     nav.style.display = "none";
-            //   } else {
-            //     nav.style.display = "block";
-            //   }
-            // }
-// function openDiv() {
-//   let div = document.getElementById("content");
-//   if (div.style.display === "block") {
-//     div.style.display = "none";
-//   } else {
-//     div.style.display = "block"
-//   }
-// }
-
-//with help from https://www.webhostingsecretrevealed.net
-// let images = new Array();
-// let preloadImages = () => {
-//   for (let i=0; i < preloadImages.arguments.length; i++){
-//        images[i] = new Image();
-//       images[i].src = preloadImages.arguments[i];
-//   }
-// }
-// preloadImages("/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMG_0782.jpg", "/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMG_0035.JPG", "/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMG_3084.jpeg", "/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMG_3248.jpeg" , "/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMGP4637.JPG" , "/Users/vanessa_nieto/Public/Portfolio/Home/IMG/IMG_7010.JPG");
-////
+// with help from https://perishablepress.com
+//see if there's a way to adopt a function for each page
+// const images = new Array()
+// 			function preload() {
+// 				for (i = 0; i < preload.arguments.length; i++) {
+// 					images[i] = new Image()
+// 					images[i].src = preload.arguments[i]
+// 				}
+// 			}
+// 			preload(
+// 				"IMG/IMG_0782.jpg",
+// 				"IMG/IMG_0035.JPG",
+// 				"IMG/IMG_3084.jpeg",
+//         "IMG/IMG_3248.jpeg",
+//         "IMG/IMGP4637.JPG",
+//         "IMG/IMG_7010.JPG"
+// 			)
